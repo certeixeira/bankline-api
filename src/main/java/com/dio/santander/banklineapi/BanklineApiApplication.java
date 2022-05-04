f61108ea48b3804f73c2920ff9030be42d89e453
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2022-05-04 17:19:52 -0300

    ajustando detalhes para o front end

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -8,6 +8,8 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		//https://certeixeira-dio-bankline-api.herokuapp.com/swagger-ui/index.html#/
+		//http://localhost:8080/swagger-ui/index.html
 	}
 
 }
