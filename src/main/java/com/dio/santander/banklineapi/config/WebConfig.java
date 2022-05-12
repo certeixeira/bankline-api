3fbe3b391567b5bb932987af2fc7a7f186b9c307
carlos <certeixeira@outlook.com>
2022-05-11 22:27:16 -0300

    Update WebConfig.java

--- src/main/java/com/dio/santander/banklineapi/config/WebConfig.java
+++ src/main/java/com/dio/santander/banklineapi/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.dio.santander.banklineapi.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
